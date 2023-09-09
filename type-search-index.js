5f90967f899a32ebcee6c957924531d808dd6ad5
gnmyt <germannewsmaker@gmail.com>
2023-09-09 20:08:26 +0000

    Deploying to javadoc from @ LicenseAPI/java-integration@a426a72e5059e7a80624804ead72a705c393b603 🚀

---
+++ type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"de.licenseapi.entities","l":"License"},{"p":"de.licenseapi.entities","l":"LicenseMeta"},{"p":"de.licenseapi.entities","l":"LicenseStatus"},{"p":"de.licenseapi","l":"LicenseValidator"}]
